c4cd498254c2159b60ba9fa14b6f3e1c1eef5748
Daniel Suter <suter_daniel@hotmail.com>
2015-01-12 18:49:37 +0100

    Added grunt task runner.
    Use with grunt watch.

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+    
+        simplemocha: {
+            options: {
+                globals: ['expect'],
+                timeout: 3000,
+                ignoreLeaks: false
+            },
+            all: { src: ['test/*.js'] }
+        },
+	watch: {
+	      lib: {
+		files: 'lib/**/*.js',
+		tasks: ['simplemocha']
+	      },
+	      test: {
+		files: 'test/**/*.js',
+		tasks: ['simplemocha']
+	      }
+	}
+    });
+ 
+    grunt.loadNpmTasks('grunt-simple-mocha');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['simplemocha']);
+};
