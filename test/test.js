4c49b536a1d55474035fcb5c334f33a7b94f9abe
Daniel Suter <suter_daniel@hotmail.com>
2015-01-13 21:21:05 +0100

    Fix some jshint errors.

--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
-var assert = require("assert"); // node.js core module
-var card = require("../lib/game/card.js")
+"use strict";
+
+//var assert = require("assert"); // node.js core module
+var card = require("../lib/game/card.js");
 
 
 describe('Game', function(){
+
     describe('Card', function(){
         it('Create a new card', function(){
-            var myCard = new card.Card(2, card.CardType.HEARTS);
-        })
-    })
+            new card.Card(2, card.CardType.HEARTS);
+        });
+    });
 });
