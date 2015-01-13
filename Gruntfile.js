230df6b4385a623ee692208c9bf1886e32b06bb6
Daniel Suter <suter_daniel@hotmail.com>
2015-01-13 21:20:52 +0100

    Add subfolders for testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,28 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-    
+
         simplemocha: {
             options: {
                 globals: ['expect'],
                 timeout: 3000,
                 ignoreLeaks: false
             },
-            all: { src: ['test/*.js'] }
+            all: {src: ['test/**/*.js']}
         },
-	watch: {
-	      lib: {
-		files: 'lib/**/*.js',
-		tasks: ['simplemocha']
-	      },
-	      test: {
-		files: 'test/**/*.js',
-		tasks: ['simplemocha']
-	      }
-	}
+        watch: {
+            lib: {
+                files: 'lib/**/*.js',
+                tasks: ['simplemocha']
+            },
+            test: {
+                files: 'test/**/*.js',
+                tasks: ['simplemocha']
+            }
+        }
     });
- 
+
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['simplemocha']);
