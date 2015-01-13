599ebf61c8159a06bdceb0daffcd202fb0a700a8
Daniel Suter <suter_daniel@hotmail.com>
2015-01-13 21:34:41 +0100

    Added jshint to grunt. Although the config does not work fully yet.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,10 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        jshint: {
+            jshintrc: ".jshintrc",
+            all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
+        },
         simplemocha: {
             options: {
                 globals: ['expect'],
@@ -25,5 +29,6 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.registerTask('default', ['simplemocha']);
 };
