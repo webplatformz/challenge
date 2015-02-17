3b5fa206e5956dde97950914685f21f192809dc0
nick <nick.payne89@gmail.com>
2015-02-17 17:29:21 +0100

    introduce mockery

--- test/game/gameTest.js
+++ test/game/gameTest.js
@@ -1,3 +1,21 @@
-/**
- * Created by dev on 2/17/15.
- */
+"use strict";
+
+//var assert = require("assert"); // node.js core module
+var mockery = require('mockery');
+
+var deckMock = {
+    getShuffledCards : function() {
+        console.log('hello mockery');
+    }
+};
+
+describe('Game', function() {
+    mockery.enable();
+    mockery.registerMock('./deck/deck', deckMock);
+    var game = Object.create(require('../../lib/game/game'));
+
+
+    it('should deal the cards properly', function() {
+        game.init();
+    });
+});
