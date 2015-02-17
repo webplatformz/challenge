79ec4ca4ad9eb8d9fa685e2eec36fc763cda51f1
Joachim Prinzbach <Joachim@clash.local>
2015-02-17 09:07:26 +0100

    Fixed typo.

--- test/server/serverTest.js
+++ test/server/serverTest.js
@@ -6,6 +6,7 @@ var webSocket = require('ws');
 
 // more help on async code http://lostechies.com/derickbailey/2012/08/17/asynchronous-unit-tests-with-mocha-promises-and-winjs/
 
+// TODO Server shoukd be started by grunt test task - test should not be relying on running server
 describe('Integration tests', function(){
     describe('Player joining', function(){
         it('should ask for a player name, on connection establish', function(done){
