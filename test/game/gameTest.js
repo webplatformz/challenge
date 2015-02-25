0d1acd9f516f1cf18b40b24a241278838f9881e2
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2015-02-25 21:45:00 +0100

    Use sinon to spy method calls in gameTest.

--- test/game/gameTest.js
+++ test/game/gameTest.js
@@ -3,6 +3,7 @@
 var assert = require("assert"); // node.js core module
 var mockery = require('mockery');
 var Card = require('../../lib/game/deck/card');
+var sinon = require('sinon');
 
 var deckMock = {
     getShuffledCards : function() {
@@ -19,11 +20,32 @@ describe('Game', function() {
         warnOnUnregistered: false
     });
     mockery.registerMock('./deck/deck', deckMock);
-    var game = Object.create(require('../../lib/game/game'));
+    var game;
+    beforeEach(function(){
+        game = Object.create(require('../../lib/game/game'));
+        sinon.spy(deckMock, 'getShuffledCards');
+    });
+
+    afterEach(function() {
+       deckMock.getShuffledCards.restore();
+    });
 
 
     it('should deal the cards properly', function() {
         game.init();
+        assert(deckMock.getShuffledCards.called);
+    });
 
+    it('should send a command to requestTrump', function() {
+        game.init();
+        game.chooseTrump();
+        // assert that request trump command has been send
+    });
+
+    it('should send a command to requestTrump to the next player after the first player pushed', function() {
+        game.init();
+        game.chooseTrump();
+        game.pushTrumpChoice();
+        // assert that push command has been send
     });
 });
