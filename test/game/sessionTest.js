2e27472d47b9313a910096559d4c7944ca71b947
r3dDoX <patrick@walther-online.ch>
2015-06-29 16:40:52 +0200

    add tests for game creation

--- test/game/sessionTest.js
+++ test/game/sessionTest.js
@@ -4,6 +4,7 @@ let expect = require('chai').expect;
 let sinon = require('sinon');
 let Session = require('../../lib/game/session');
 let ClientApi = require('../../lib/communication/clientApi');
+let Game = require('../../lib/game/game');
 
 describe('Session', function() {
     let session;
@@ -48,22 +49,47 @@ describe('Session', function() {
     });
 
     describe('startGame', () => {
-        it('should create clientApi with webSockets array', () => {
-            let clientApiMock = sinon.mock(ClientApi),
-                clients = ['ws1','ws2', 'ws3', 'ws4'];
-            clientApiMock.expects("create").withArgs(clients).once();
+        it('should fail if session is not complete', () => {
+             expect(() => { session.startGame(); }).to.throw('Not enough players to start game!');
+        });
+    });
+
+    describe('startGame', () => {
+
+        let clientApiMock,
+            gameMock,
+            clients = ['ws1','ws2', 'ws3', 'ws4'];
+
+        beforeEach(() => {
+            clientApiMock = sinon.mock(ClientApi);
+            gameMock = sinon.mock(Game);
 
             session.addPlayer(clients[0]);
             session.addPlayer(clients[1]);
             session.addPlayer(clients[2]);
             session.addPlayer(clients[3]);
+        });
+
+        it('should create new clientApi and new game when first game is started', () => {
+            var clientApiInstance = {clientApiInstance: 'instance'};
+            clientApiMock.expects('create').withArgs(clients).returns(clientApiInstance).once();
+            gameMock.expects('create').withArgs(session.players, session.maxPoints, session.players[0], clientApiInstance).once();
+
             session.startGame();
 
             clientApiMock.verify();
+            gameMock.verify();
         });
 
-        it('should fail if session is not complete', () => {
-             expect(() => { session.startGame(); }).to.throw('Not enough players to start game!');
+        it('should use created client api and create new game when second game is started', () => {
+            var clientApiInstance = {clientApiInstance: 'instance'};
+            clientApiMock.expects('create').withArgs(clients).returns(clientApiInstance).never();
+            gameMock.expects('create').withArgs(session.players, session.maxPoints, session.players[1], clientApiInstance).once();
+
+            session.startGame();
+
+            clientApiMock.verify();
+            gameMock.verify();
         });
     });
 });
