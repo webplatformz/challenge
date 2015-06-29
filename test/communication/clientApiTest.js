078f925431593e491c438f4805e8bf9e01d29265
r3dDoX <patrick@walther-online.ch>
2015-06-29 18:56:57 +0200

    add integration test for broadcastPlayedCards

--- test/communication/clientApiTest.js
+++ test/communication/clientApiTest.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let assert = require('assert');
+let expect = require('chai').expect;
 let WebSocket = require('ws');
 let WebSocketServer = require('ws').Server;
 let ClientApi = require('../../lib/communication/clientApi');
@@ -8,10 +8,12 @@ let messages = require('../../lib/communication/messages');
 
 describe('Client API', () => {
 
-    let wss;
+    let wss,
+        clientApi;
 
     beforeEach(() => {
         wss = new WebSocketServer({port: 10001});
+        clientApi = ClientApi.create();
     });
 
     afterEach(() => {
@@ -20,25 +22,23 @@ describe('Client API', () => {
 
     describe('addClient', () => {
         it('should add given client to clients array', () => {
-            let client = 'client',
-                clientApi = ClientApi.create();
+            let client = 'client';
 
             clientApi.addClient(client);
 
-            assert.equal(client, clientApi.clients[0]);
+            expect(clientApi.clients[0]).to.equal(client);
         });
     });
 
     describe('requestTrumpf', () => {
         it('should wait for chooseTrumpf', (done) => {
-            let chooseTrump = messages.create(messages.MessageType.CHOOSE_TRUMP, 'Spades'),
-                clientApi = ClientApi.create();
+            let chooseTrump = messages.create(messages.MessageType.CHOOSE_TRUMPF, 'Spades');
 
             wss.on('connection', (client) => {
                 clientApi.addClient(client);
 
                 clientApi.requestTrumpf(client, false).then((data) => {
-                    assert.equal(data.color, chooseTrump.data.color);
+                    expect(data.color).to.equal(chooseTrump.data.color);
                     done();
                 }).catch(done);
             });
@@ -48,11 +48,45 @@ describe('Client API', () => {
             client.on('message', (message) => {
                 message = JSON.parse(message);
 
-                if (message.type === messages.MessageType.REQUEST_TRUMP) {
+                if (message.type === messages.MessageType.REQUEST_TRUMPF) {
                     client.send(JSON.stringify(chooseTrump));
                 }
             });
         });
     });
 
+    describe('broadcastPlayedCards', () => {
+        it('should send played cards to all clients', (done) => {
+            let clients,
+                cardsPlayed = ['a', 'b', 'c'],
+                clientPromises = [];
+
+            wss.on('connection', (client) => {
+                clientApi.addClient(client);
+
+                if (clientApi.clients.length === clients.length) {
+                    clientApi.broadcastCardPlayed(cardsPlayed);
+                }
+            });
+
+            clients = [new WebSocket('ws://localhost:10001'), new WebSocket('ws://localhost:10001')];
+
+            clients.forEach((client) => {
+                clientPromises.push(new Promise((resolve) => {
+                    client.on('message', (message) => {
+                        message = JSON.parse(message);
+
+                        expect(message.type).to.equal(messages.MessageType.CARDS_PLAYED);
+                        expect(message.data.cardsPlayed).to.eql(cardsPlayed);
+
+                        resolve();
+                    });
+                }));
+            });
+
+            Promise.all(clientPromises).then(() => {
+                done();
+            }).catch(done);
+        });
+    });
 });
