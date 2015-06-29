64bea96c4d568d5d8d037a24d9fdf9d93feaf0a4
r3dDoX <patrick@walther-online.ch>
2015-06-29 10:13:37 +0200

    refactore deck and tests

--- test/game/deck/deckTest.js
+++ test/game/deck/deckTest.js
@@ -6,20 +6,20 @@ let CardType = require('../../../lib/game/deck/card').CardType;
 
 describe('Deck', function() {
 
-    it('has no cards on creation', function() {
-        let deck = Object.create(Deck);
+    it('has shuffled cards', () => {
+        let deck = Deck.create(),
+            deck2 = Deck.create(),
+            foundNotIdenticalCard;
 
-        assert(deck.cards === undefined);
+        assert(deck.cards !== deck2.cards, 'Cards array are the same in two instances');
+        for(let i = 0; i < deck.cards.length; i++) {
+            let card = deck.cards[i],
+                card2 = deck2.cards[i];
+
+            if (card.number !== card2.number || card.type !== card2.type) {
+                foundNotIdenticalCard = true;
+            }
+        }
+        assert(foundNotIdenticalCard, 'cards in deck not shuffled. Two deck instances have same ordered cards');
     });
-
-    it('gets instance specific shuffled deck on shuffleCards()', function() {
-        let deck = Object.create(Deck),
-            deck2 = Object.create(Deck);
-
-        deck.shuffleCards();
-        deck2.shuffleCards();
-
-        assert(deck.cards !== deck2.cards, 'Cards array is not the same in prototype');
-    });
-
 });
