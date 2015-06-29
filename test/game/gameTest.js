9e92ff2192e3759f8ba2594fbcac14d4e3fd762a
nick <nick.payne89@gmail.com>
2015-06-29 15:19:16 +0200

    Added id and name property to player.

--- test/game/gameTest.js
+++ test/game/gameTest.js
@@ -1,47 +1,40 @@
 "use strict";
 
-let assert = require("assert"); // node.js core module
-let Game = require('../../lib/game/game');
-let mockery = require('mockery');
-let sinon = require('sinon');
-
-let clientApiMock = {
-    requestTrumpf: function () {
-        return true;
-    }
-};
+let assert      = require("assert"); // node.js core module
+let Game        = require('../../lib/game/game');
+let clientApi   = require('../../lib/communication/clientApi').create();
+let sinon       = require('sinon');
 
 describe('Game', function () {
     let maxPoints = 2500;
     let game;
-    initMock();
+    let clientApiMock;
 
     beforeEach(function () {
-        sinon.stub(clientApiMock, 'requestTrumpf');
-        game = Game.create([], maxPoints, 'dummyPlayer', clientApiMock);
+        clientApiMock = sinon.mock(clientApi);
     });
 
     it('should have a properly initialized deck', () => {
+        clientApiMock.expects('requestTrump').once();
+        game = Game.create([], maxPoints, 'dummyPlayer', clientApi);
+        clientApiMock.verify();
+
         assert.notEqual(undefined, game.deck);
         assert.notEqual(undefined, game.players);
         assert.equal(maxPoints, game.maxPoints);
         assert.notEqual(undefined, game.startPlayer);
     });
 
-    it('should request trumpf after initialization', () => {
-        sinon.assert.calledOnce(clientApiMock.requestTrumpf);
+    it('should request the trumpf from the correct player', () => {
+        clientApiMock.expects('requestTrump').once()
+            .withArgs(0, false);
+        game = Game.create([], maxPoints, 'dummyPlayer', clientApi);
+        clientApiMock.verify();
     });
 
     afterEach(function () {
-        clientApiMock.requestTrumpf.restore();
+        clientApiMock.restore();
     });
 
-
-    function initMock() {
-        mockery.enable({
-            warnOnUnregistered: false
-        });
-        mockery.registerMock('./deck/deck', clientApiMock);
-    }
 });
 
