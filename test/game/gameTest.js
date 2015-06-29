d1887034775202f50df62d8a29fd4805a91c51da
nick <nick.payne89@gmail.com>
2015-06-29 18:13:41 +0200

    use promises for requesting trumpf

--- test/game/gameTest.js
+++ test/game/gameTest.js
@@ -4,6 +4,7 @@ let assert = require("assert"); // node.js core module
 let Game = require('../../lib/game/game');
 let Player = require('../../lib/game/player/player');
 let Card = require('../../lib/game/deck/card');
+let Cycle = require('../../lib/game/cycle/cycle');
 let clientApi = require('../../lib/communication/clientApi').create();
 let sinon = require('sinon');
 
@@ -28,7 +29,7 @@ describe('Game', function () {
 
     it('should request the trumpf from the correct player', () => {
         clientApiMock.expects('requestTrumpf').once()
-            .withArgs(false);
+            .withArgs(false).returns(Promise.resolve());
 
         game = Game.create([], maxPoints, player, clientApi);
         game.start();
@@ -36,23 +37,32 @@ describe('Game', function () {
         clientApiMock.verify();
     });
 
-    it('should save and broadcast the trumpf when it has been chosen from the player', () => {
-        clientApiMock.expects('requestTrumpf').once();
-        game = Game.create([], maxPoints, player, clientApi);
-        game.start();
-
-        clientApiMock.verify();
+    it('should save and broadcast the trumpf when it has been chosen from the player', (done) => {
         let gameMode = Game.GameMode.TRUMPF;
-
         let cardColor = Card.CardType.HEARTS;
         let gameType = Game.GameType.create(gameMode, cardColor);
 
-        clientApiMock.expects('broadcastTrumpf').once();
-        game.chooseTrumpf(gameType);
+        clientApiMock.expects('requestTrumpf').once()
+            .returns(Promise.resolve(gameType));
 
-        assert.equal(cardColor, game.gameType.trumpfColor);
-        assert.equal(gameMode, game.gameType.mode);
+        game = Game.create([], maxPoints, player, clientApi);
+        game.start();
         clientApiMock.verify();
+
+        clientApiMock.expects('broadcastTrumpf').once();
+        setTimeout(() => {
+            assert.equal(cardColor, game.gameType.trumpfColor);
+            assert.equal(gameMode, game.gameType.mode);
+            clientApiMock.verify();
+            done();
+        }, 10);
+    });
+
+    it('should start the first cycle after trumpf has been chosen', () => {
+        game = Game.create([], maxPoints, player, clientApi);
+        game.gameType = Game.GameType.create(Game.GameMode.TRUMPF, Card.CardType.HEARTS);
+
+        game.nextCycle();
     });
 
     afterEach(function () {
