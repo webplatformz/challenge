b51769877321f05ed8f7861793b1a6258ed95c20
joachimprinzbach <joachim.prinzbach@gmail.com>
2015-06-30 08:58:28 +0200

    Added cycleCounter to decide whose card has won a cycle.

---
+++ test/game/counter/cycleCounterTest.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let assert = require("assert");
+let GameMode = require('../../../lib/game/game').GameMode;
+let CycleCounter = require('../../../lib/game/cycle/cycleCounter');
+let CardType = require('../../../lib/game/deck/card').CardType;
+let Card = require('../../../lib/game/deck/card');
+
+
+describe('CycleCounter', function() {
+    it('should count simple array without Trumpf', function() {
+        let mode = GameMode.TRUMPF;
+        let cardType = CardType.SPADES;
+
+        let winnerCard = Card.create(14, CardType.DIAMONDS);
+
+        let cardSet = [
+            Card.create(6, CardType.DIAMONDS),
+            Card.create(12, CardType.DIAMONDS),
+            winnerCard,
+            Card.create(10, CardType.DIAMONDS)];
+
+        let result = CycleCounter.count(mode, cardType, cardSet);
+        assert.equal(winnerCard, result);
+    });
+});
