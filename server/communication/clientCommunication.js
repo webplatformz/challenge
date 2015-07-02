f8379cc6c6405011e360bd048e7d634782acad88
r3dDoX <patrick@walther-online.ch>
2015-07-03 00:39:07 +0200

    fix memory leak

--- server/communication/clientCommunication.js
+++ server/communication/clientCommunication.js
@@ -28,9 +28,7 @@ function request(client, messageType, onMessage, ...data) {
     client.send(toJSON(messages.create(messageType, ...data)));
 
     return new Promise((resolve, reject) => {
-        client.on('message', (message) => {
-            onMessage(message, resolve, reject);
-        });
+        client.on('message', onMessage.bind(null, resolve, reject));
     });
 }
 
