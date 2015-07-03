cdabb8a56d92e7a46bbb4026e3684b3d8b61ae98
danielsuter <suter_daniel@hotmail.com>
2015-07-03 02:26:52 +0200

    added helper methods

--- test/server/communication/clientApiIntegrationTest.js
+++ test/server/communication/clientApiIntegrationTest.js
@@ -16,34 +16,6 @@ let messages = require('../../../shared/messages/messages');
 
 let trumpf = CardColor.SPADES;
 
-let giveValidCardFromHand = function(tableCards, handCards) {
-    let cardToPlay;
-
-    let validation = Validation.create(GameMode.TRUMPF, trumpf);
-    handCards.forEach((handCard) => {
-        if(validation.validate(tableCards, handCards, handCard)) {
-            cardToPlay = handCard;
-        }
-    });
-
-    return cardToPlay;
-};
-
-
-let choosePlayerName = (name) => {
-    return messages.create(messages.MessageType.CHOOSE_PLAYER_NAME, name);
-};
-
-let mapCardsFromJson = function(cards) {
-    return cards.map((element) => {
-        return Card.create(element.number, element.color);
-    });
-};
-
-function createClient() {
-    return new WebSocket('ws://localhost:10001');
-}
-
 let Client = {
     handcards : [],
     name: [],
@@ -52,11 +24,11 @@ let Client = {
         let message = JSON.parse(messageJson);
 
         if (message.type === messages.MessageType.REQUEST_PLAYER_NAME) {
-            this.client.send(JSON.stringify(choosePlayerName(this.name)));
+            this.client.send(JSON.stringify(messages.create(messages.MessageType.CHOOSE_PLAYER_NAME, this.name)));
         }
 
         if (message.type === messages.MessageType.DEAL_CARDS) {
-            this.handcards = mapCardsFromJson(message.data);
+            this.handcards = this.mapCardsFromJson(message.data);
         }
 
         if (message.type === messages.MessageType.BROADCAST_WINNER_TEAM) {
@@ -64,7 +36,7 @@ let Client = {
         }
 
         if (message.type === messages.MessageType.REQUEST_CARD) {
-            let handCard = giveValidCardFromHand(mapCardsFromJson(message.data), this.handcards);
+            let handCard = this.giveValidCardFromHand(this.mapCardsFromJson(message.data), this.handcards);
             this.handcards.splice(this.handcards.indexOf(handCard), 1);
             let chooseCardResonse = messages.create(messages.MessageType.CHOOSE_CARD, handCard);
             this.client.send(JSON.stringify(chooseCardResonse));
@@ -75,6 +47,25 @@ let Client = {
             let chooseTrumpfResponse = messages.create(messages.MessageType.CHOOSE_TRUMPF, gameType);
             this.client.send(JSON.stringify(chooseTrumpfResponse));
         }
+    },
+
+    mapCardsFromJson : function(cards) {
+        return cards.map((element) => {
+            return Card.create(element.number, element.color);
+        });
+    },
+
+    giveValidCardFromHand : function(tableCards, handCards) {
+        let cardToPlay;
+
+        let validation = Validation.create(GameMode.TRUMPF, trumpf);
+        handCards.forEach((handCard) => {
+            if(validation.validate(tableCards, handCards, handCard)) {
+                cardToPlay = handCard;
+            }
+        });
+
+        return cardToPlay;
     },
 
     create: function create(name, doneFunction) {
