105035f9363da4c721e73576cd36340997995b0b
r3dDoX <patrick@walther-online.ch>
2015-07-06 10:35:31 +0200

    refactor Player to prototype

--- server/game/player/player.js
+++ server/game/player/player.js
@@ -1,36 +1,37 @@
 'use strict';
 
+let Player = {
+    dealCards: function dealCards(cards) {
+        this.cards = cards;
+        return this.clientApi.dealCards(cards);
+    },
+
+    rejectCard: function rejectCard(card, cardsOnTable) {
+        return this.clientApi.rejectCard(card, cardsOnTable);
+    },
+
+    requestCard: function requestCard(cardsOnTable) {
+        return this.clientApi.requestCard(cardsOnTable);
+    },
+
+    requestTrumpf: function (isGeschoben) {
+        return this.clientApi.requestTrumpf(isGeschoben);
+    },
+
+    removeCard: function removeCard(cardToRemove) {
+        this.cards = this.cards.filter((card) => {
+            return !card.equals(cardToRemove);
+        });
+    }
+};
+
 let create = function create(team, name, id, clientApi) {
-    return {
-        team,
-        name,
-        id,
-        clientApi,
-        cards : [],
-
-        dealCards: function dealCards(cards) {
-            this.cards = cards;
-            return this.clientApi.dealCards(cards);
-        },
-
-        rejectCard: function rejectCard(card, cardsOnTable) {
-            return this.clientApi.rejectCard(card, cardsOnTable);
-        },
-
-        requestCard: function requestCard(cardsOnTable) {
-            return this.clientApi.requestCard(cardsOnTable);
-        },
-
-        requestTrumpf: function (isGeschoben) {
-            return this.clientApi.requestTrumpf(isGeschoben);
-        },
-
-        removeCard: function removeCard(cardToRemove) {
-            this.cards = this.cards.filter((card) => {
-                return !card.equals(cardToRemove);
-            });
-        }
-    };
+    let player = Object.create(Player);
+    player.id = id;
+    player.name = name;
+    player.team = team;
+    player.clientApi = clientApi;
+    return player;
 };
 
 module.exports = {
