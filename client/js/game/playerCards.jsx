1305810b82a6568119aca63b6e134a8797967bf0
r3dDoX <patrick@walther-online.ch>
2015-08-08 10:35:44 +0200

    order playerCards to get better readability

--- client/js/game/playerCards.jsx
+++ client/js/game/playerCards.jsx
@@ -1,6 +1,15 @@
 'use strict';
 
-let React = require('react');
+let React = require('react'),
+    CardColor = require('../../../shared/deck/card').CardColor;
+
+let colorIndices = {};
+
+Object.getOwnPropertyNames(CardColor).forEach((color, index) => {
+    colorIndices[color] = index;
+});
+
+console.log(colorIndices);
 
 module.exports = React.createClass({
 
@@ -13,11 +22,17 @@ module.exports = React.createClass({
 
         return (
             <div id="playerCards">
-                {cards.map(function(card) {
+                {cards.sort((a, b) => {
+                    if (a.color !== b.color) {
+                        return colorIndices[a.color] - colorIndices[b.color];
+                    }
+
+                    return a.number - b.number;
+                }).map((card) => {
                     return (
                         <img key={card.color + '-' + card.number} src={'/images/cards/' + this.props.cardType + '/' + card.color + '_' + card.number + '.gif'}
                             onClick={this.playCard(card.color, card.number)} />);
-                }.bind(this))}
+                })}
             </div>
         );
     }
