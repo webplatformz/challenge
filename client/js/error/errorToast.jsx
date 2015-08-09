1f13a35035f85f68560c1c2a6eef07cfcba57571
r3dDoX <patrick@walther-online.ch>
2015-08-09 22:23:24 +0200

    add test for the error toast

--- client/js/error/errorToast.jsx
+++ client/js/error/errorToast.jsx
@@ -4,9 +4,11 @@ let React = require('react');
 
 module.exports = React.createClass({
     render: function () {
+        let error = this.props.error || '';
+
         return (
             <div id="errorToast" className={(this.props.error) ? 'in' : 'out'}>
-                <span>{String(this.props.error)}</span>
+                <span>{String(error)}</span>
             </div>
         )
     }
