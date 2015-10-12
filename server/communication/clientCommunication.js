2b1334fd996fc81e7526114d4b85752a1e1606ae
r3dDoX <patrick@walther-online.ch>
2015-10-12 22:24:11 +0200

    add new await function communication to wait for a specific message

--- server/communication/clientCommunication.js
+++ server/communication/clientCommunication.js
@@ -1,53 +1,68 @@
 'use strict';
 
-let messages = require('../../shared/messages/messages');
-let Logger = require('./logger');
+import messages from '../../shared/messages/messages.js';
+import MessageType from '../../shared/messages/messageType.js';
+import validate from 'validate.js';
+import Logger from './logger.js';
 
-function toJSON(object) {
-    return JSON.stringify(object);
-}
+export default {
+    toJSON(object) {
+        return JSON.stringify(object);
+    },
 
-function fromJSON(jsonAsString) {
-    try {
-        return JSON.parse(jsonAsString);
-    } catch (error) {
-        Logger.error("Bad message from client: " + jsonAsString);
-    }
-}
+    fromJSON(jsonAsString) {
+        try {
+            return JSON.parse(jsonAsString);
+        } catch (error) {
+            Logger.error("Bad message from client: " + jsonAsString);
+        }
+    },
 
-function send(client, messageType, ...data) {
-    var messageToSend = toJSON(messages.create(messageType, ...data));
-    Logger.debug('<-- Send Message: ' + messageToSend);
-    client.send(messageToSend);
-}
+    await(client, expectedMessageType) {
+        return new Promise((resolve, reject) => {
+            client.on('message', (message) => {
+                let messageObject = this.fromJSON(message);
 
-function broadcast(clients, messageType, ...data) {
-    Logger.debug('<-- Start Broadcast: ');
-    clients.forEach((client) => {
-        send(client, messageType, ...data);
-    });
-    Logger.debug('End Broadcast -->');
+                if (messageObject.type === expectedMessageType.name) {
+                    let validationResult = validate(messageObject, expectedMessageType.constraints);
 
-}
-
-function request(client, messageType, onMessage, ...data) {
-    var messageToSend = toJSON(messages.create(messageType, ...data));
-    client.send(messageToSend);
-    Logger.debug('<-- Send Message: ' + messageToSend);
-
-    return new Promise((resolve, reject) => {
-        client.on('message', function handleMessage(message) {
-            Logger.debug('<-- Received Message: ' + message);
-            client.removeListener('message', handleMessage);
-            onMessage(message, resolve, reject);
+                    if (validationResult) {
+                        console.log(validationResult);
+                        this.send(client, MessageType.BAD_MESSAGE.name, validationResult);
+                        reject(validationResult);
+                    } else {
+                        resolve(messageObject);
+                    }
+                }
+            });
         });
-    });
-}
+    },
 
-module.exports = {
-    toJSON,
-    fromJSON,
-    send,
-    broadcast,
-    request
+    send(client, messageType, ...data) {
+        var messageToSend = this.toJSON(messages.create(messageType, ...data));
+        Logger.debug('<-- Send Message: ' + messageToSend);
+        client.send(messageToSend);
+    },
+
+    broadcast(clients, messageType, ...data) {
+        Logger.debug('<-- Start Broadcast: ');
+        clients.forEach((client) => {
+            this.send(client, messageType, ...data);
+        });
+        Logger.debug('End Broadcast -->');
+    },
+
+    request(client, messageType, onMessage, ...data) {
+        var messageToSend = this.toJSON(messages.create(messageType, ...data));
+        client.send(messageToSend);
+        Logger.debug('<-- Send Message: ' + messageToSend);
+
+        return new Promise((resolve, reject) => {
+            client.on('message', function handleMessage(message) {
+                Logger.debug('<-- Received Message: ' + message);
+                client.removeListener('message', handleMessage);
+                onMessage(message, resolve, reject);
+            });
+        });
+    }
 };
--- test/server/communication/clientCommunicationTest.js
+++ test/server/communication/clientCommunicationTest.js
@@ -4,83 +4,153 @@ let clientCommunication = require('../../../server/communication/clientCommunica
     WebSocket = require('ws'),
     messages = require('../../../shared/messages/messages'),
     MessageType = require('../../../shared/messages/messageType'),
+    CardColor = require('../../../shared/deck/card').CardColor,
     expect = require('chai').expect,
     sinon = require('sinon');
 
 describe('clientCommunication', () => {
-    it('should convert message to JSON string', () => {
-        let message = messages.create(MessageType.REQUEST_TRUMPF.name, false);
+    describe('toJSON', () => {
+        it('should convert message to JSON string', () => {
+            let message = messages.create(MessageType.REQUEST_TRUMPF.name, false);
 
-        let actual = clientCommunication.toJSON(message);
+            let actual = clientCommunication.toJSON(message);
 
-        expect(actual).to.equal('{"type":"REQUEST_TRUMPF","data":false}');
+            expect(actual).to.equal('{"type":"REQUEST_TRUMPF","data":false}');
+        });
     });
 
-    it('should convert JSON string to message', () => {
-        let message = '{"type":"REQUEST_TRUMPF","data":false}';
+    describe('fromJSON', () => {
+        it('should convert JSON string to message', () => {
+            let message = '{"type":"REQUEST_TRUMPF","data":false}';
 
-        let actual = clientCommunication.fromJSON(message);
+            let actual = clientCommunication.fromJSON(message);
 
-        expect(actual.type).to.equal(MessageType.REQUEST_TRUMPF.name);
-        expect(actual.data).to.eql(false);
+            expect(actual.type).to.equal(MessageType.REQUEST_TRUMPF.name);
+            expect(actual.data).to.eql(false);
+        });
     });
 
-    it('should send message to given client', () => {
-        let WebSocketStub = {
-            send: function () {
-            }
-        };
+    describe('await', () => {
+        it('should resolve on correct message only', (done) => {
+            let messageHandler,
+                wrongMessage = {
+                    type: MessageType.CHOOSE_SESSION.name
+                },
+                correctMessage = {
+                    type: MessageType.CHOOSE_CARD.name,
+                    data: {
+                        number: 9,
+                        color: CardColor.HEARTS
+                    }
+                },
+                WebSocketStub = {
+                    on: function (type, handlerFunction) {
+                        messageHandler = handlerFunction;
+                    }
+                };
 
-        let client = Object.create(WebSocketStub),
-            clientMock = sinon.mock(client);
+            let client = Object.create(WebSocketStub);
 
-        clientMock.expects('send').withExactArgs('{"type":"DEAL_CARDS","data":["a","b","c"]}').once();
+            let actual = clientCommunication.await(client, MessageType.CHOOSE_CARD);
 
-        clientCommunication.send(client, MessageType.DEAL_CARDS.name, ['a', 'b', 'c']);
+            messageHandler(JSON.stringify(wrongMessage));
+            messageHandler(JSON.stringify(correctMessage));
 
-        clientMock.verify();
+            actual.then((message) => {
+                expect(message).not.to.eql(wrongMessage);
+                expect(message).to.eql(correctMessage);
+                done();
+            }).catch(done);
+        });
+
+        it('should reject on correct message type but invalid message', (done) => {
+            let messageHandler,
+                invalidMessage = {
+                    type: MessageType.CHOOSE_PLAYER_NAME.name
+                },
+                WebSocketStub = {
+                    on(type, handlerFunction) {
+                        messageHandler = handlerFunction;
+                    },
+                    send: sinon.spy()
+                };
+
+            let client = Object.create(WebSocketStub);
+
+            let actual = clientCommunication.await(client, MessageType.CHOOSE_PLAYER_NAME);
+
+            messageHandler(JSON.stringify(invalidMessage));
+
+            actual.then(() => done(new Error('Invalid message should be rejected!')), (validationResult) => {
+                expect(validationResult).to.have.property('data').that.is.an('array');
+                expect(WebSocketStub.send.calledOnce).to.equal(true);
+                done();
+            }).catch(done);
+        });
     });
 
-    it('should broadcast message to all given clients', () => {
-        let WebSocketStub = {
-            send: function () {
-            }
-        };
-
-        let client1 = Object.create(WebSocketStub),
-            client2 = Object.create(WebSocketStub),
-            client1Mock = sinon.mock(client1),
-            client2Mock = sinon.mock(client2);
-
-        client1Mock.expects('send').withExactArgs('{"type":"PLAYED_CARDS","data":["a","b","c"]}').once();
-        client2Mock.expects('send').withExactArgs('{"type":"PLAYED_CARDS","data":["a","b","c"]}').once();
-
-        clientCommunication.broadcast([client1, client2], MessageType.PLAYED_CARDS.name, ['a', 'b', 'c']);
-
-        client1Mock.verify();
-        client2Mock.verify();
-    });
-
-    it('should send request message to given client and call given function on answer', (done) => {
-        let client = {
+    describe('send', ()=> {
+        it('should send message to given client', () => {
+            let WebSocketStub = {
                 send: function () {
-                },
-                on: function (message, onMessage) {
-                    onMessage(message);
-                },
-                removeListener: function() {
                 }
-            },
-            clientMock = sinon.mock(client);
+            };
 
-        clientMock.expects('send').withExactArgs('{"type":"REQUEST_TRUMPF","data":false}').once();
-        clientMock.expects('removeListener').withArgs('message', sinon.match.func).once();
+            let client = Object.create(WebSocketStub),
+                clientMock = sinon.mock(client);
 
-        clientCommunication.request(client, MessageType.REQUEST_TRUMPF.name, function onMessage(message, resolve) {
-            resolve();
+            clientMock.expects('send').withExactArgs('{"type":"DEAL_CARDS","data":["a","b","c"]}').once();
+
+            clientCommunication.send(client, MessageType.DEAL_CARDS.name, ['a', 'b', 'c']);
+
             clientMock.verify();
-            done();
-        }, false);
+        });
+    });
 
+    describe('broadcast', () => {
+        it('should broadcast message to all given clients', () => {
+            let WebSocketStub = {
+                send: function () {
+                }
+            };
+
+            let client1 = Object.create(WebSocketStub),
+                client2 = Object.create(WebSocketStub),
+                client1Mock = sinon.mock(client1),
+                client2Mock = sinon.mock(client2);
+
+            client1Mock.expects('send').withExactArgs('{"type":"PLAYED_CARDS","data":["a","b","c"]}').once();
+            client2Mock.expects('send').withExactArgs('{"type":"PLAYED_CARDS","data":["a","b","c"]}').once();
+
+            clientCommunication.broadcast([client1, client2], MessageType.PLAYED_CARDS.name, ['a', 'b', 'c']);
+
+            client1Mock.verify();
+            client2Mock.verify();
+        });
+    });
+
+    describe('request', () => {
+        it('should send request message to given client and call given function on answer', (done) => {
+            let client = {
+                    send: function () {
+                    },
+                    on: function (message, onMessage) {
+                        onMessage(message);
+                    },
+                    removeListener: function () {
+                    }
+                },
+                clientMock = sinon.mock(client);
+
+            clientMock.expects('send').withExactArgs('{"type":"REQUEST_TRUMPF","data":false}').once();
+            clientMock.expects('removeListener').withArgs('message', sinon.match.func).once();
+
+            clientCommunication.request(client, MessageType.REQUEST_TRUMPF.name, function onMessage(message, resolve) {
+                resolve();
+                clientMock.verify();
+                done();
+            }, false);
+
+        });
     });
 });
