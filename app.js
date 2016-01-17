43d9258276e44048812e72de21787686fbc42555
r3dDoX <patrick@walther-online.ch>
2016-01-17 23:05:28 +0100

    remove testing tasks from grunt and handle them with npm scripts

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import express from 'express';
+import Server from './server/server';
+
+const port = process.env.PORT || 3000;
+let app = express();
+
+app.use(express.static(__dirname + '/client'));
+
+Server.start(port, app);
