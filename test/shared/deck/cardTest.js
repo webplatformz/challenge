12d733909e5a59deecc7cda9ffb1448cf4062fc7
r3dDoX <patrick@walther-online.ch>
2016-01-24 18:37:30 +0100

    add test for the equals method

--- test/shared/deck/cardTest.js
+++ test/shared/deck/cardTest.js
@@ -1,14 +1,39 @@
 "use strict";
 
-import assert from 'assert';
+import {expect} from 'chai';
 import Card from '../../../shared/deck/card';
 import CardColor from '../../../shared/deck/cardColor';
 import GameMode from '../../../shared/game/gameMode';
 
-describe('Card', function() {
-    it('should create cards', function() {
+describe('Card', () => {
+    it('should create cards', () => {
         let card = Card.create(6, CardColor.DIAMONDS);
-        assert(card.number === 6, 'Card has given number');
-        assert(card.color === CardColor.DIAMONDS, 'Card has given color');
+        expect(card.number).to.equal(6);
+        expect(card.color).to.equal(CardColor.DIAMONDS);
+    });
+
+    describe('equals', () => {
+        it('should equal on same object', () => {
+            let card = Card.create(10, CardColor.HEARTS);
+            expect(card.equals(card)).to.equal(true);
+        });
+
+        it('should equal on same number and color', () => {
+            let card1 = Card.create(12, CardColor.DIAMONDS);
+            let card2 = Card.create(12, CardColor.DIAMONDS);
+            expect(card1.equals(card2)).to.equal(true);
+        });
+
+        it('should not equal on different number', () => {
+            let card1 = Card.create(12, CardColor.DIAMONDS);
+            let card2 = Card.create(6, CardColor.DIAMONDS);
+            expect(card1.equals(card2)).to.equal(false);
+        });
+
+        it('should not equal on different color', () => {
+            let card1 = Card.create(6, CardColor.DIAMONDS);
+            let card2 = Card.create(6, CardColor.HEARTS);
+            expect(card1.equals(card2)).to.equal(false);
+        });
     });
 });
