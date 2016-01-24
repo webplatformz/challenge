77d4edab115ece66ab03b6951ce2d2356c39c860
r3dDoX <patrick@walther-online.ch>
2016-01-24 21:37:13 +0100

    move start client js

--- client/client.js
+++ client/client.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import './polyfills';
+import './js/polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import JassApp from './jassApp.jsx';
+import JassApp from './js/jassApp.jsx';
 
 /*jshint ignore:start */
 ReactDOM.render(<JassApp />, document.getElementsByTagName('body')[0]);
