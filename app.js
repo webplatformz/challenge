e1d97af959cfe5c0e4983e9f755d7f3776f197db
r3dDoX <patrick@walther-online.ch>
2016-01-31 16:45:24 +0100

    move build into client and server directory to not serve app.js statically which could be dangerous

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ import Server from './server/server';
 const port = process.env.PORT || 3000;
 let app = express();
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/build/client'));
 
 Server.start(port, app);
