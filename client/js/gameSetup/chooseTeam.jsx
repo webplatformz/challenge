85efba67d92d24e457635c4642028c12b79b0f8f
Rolf Bruderer <bruderer@computerscience.ch>
2016-06-23 09:27:35 +0200

    Improved choose team page

--- client/js/gameSetup/chooseTeam.jsx
+++ client/js/gameSetup/chooseTeam.jsx
@@ -8,39 +8,31 @@ import {SessionType} from '../../../shared/session/sessionType.js';
 
 export default React.createClass({
 
-    joinTeam1: function() {
-        let chosenSession = this.props.setupState.chosenSession;
-        chosenSession.joinSession(0);
-    },
-
-    joinTeam2: function() {
-        let chosenSession = this.props.setupState.chosenSession;
-        chosenSession.joinSession(1);
-    },
-
-    joinTeamAny: function() {
-        let chosenSession = this.props.setupState.chosenSession;
-        chosenSession.joinSession();
-    },
-
     render: function () {
         let status = this.props.setupState.status;
+        let chosenSession = this.props.setupState.chosenSession;
+        if (chosenSession) {
+            return (
 
-        return (
-            <div id="chooseTeam" className={(status !== GameSetupStore.GameSetupState.CHOOSE_TEAM ? 'hidden' : '')}>
-                <h1 className="jumbotron">Choose Your Preferred Team ... </h1>
-                <div className="team-choice">
-                    <div>
-                        <button type="button" name="team1" onClick={this.joinTeam1}>Team 1</button>
-                        <button type="button" name="team2" onClick={this.joinTeam2}>Team 2</button>
-                    </div>
-                    <div></div>
-                    <div>
-                        <button type="button" name="autoJoin" onClick={this.joinTeamAny}>Just Join Any Team</button>
+
+                <div id="chooseTeam" className={(status !== GameSetupStore.GameSetupState.CHOOSE_TEAM ? 'hidden' : '')}>
+                    <h1 className="jumbotron">Choose Your Preferred Team ... </h1>
+                    <div className="team-choice">
+                        <button type="button" name="team1" onClick={chosenSession.joinSession.bind(null, 0)}>Team 1</button>
+                        &nbsp;&nbsp;&nbsp;
+                        <button type="button" name="team2" onClick={chosenSession.joinSession.bind(null, 1)}>Team 2</button>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <button type="button" name="teamAny" onClick={chosenSession.joinSession.bind(null, undefined)}>Join Any Team</button>
+
                     </div>
                 </div>
-            </div>
-        )
+            );
+        }
+        else {
+            return (<div></div>);
+        }
     }
 
 });
