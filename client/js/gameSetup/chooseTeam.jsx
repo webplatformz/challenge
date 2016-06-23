ad3dd814943c4a954d8390a33defd55d9990b219
r3dDoX <little_mc_walther@hotmail.com>
2016-06-23 11:05:27 +0200

    remove if to prevent returning of additional empty divs

--- client/js/gameSetup/chooseTeam.jsx
+++ client/js/gameSetup/chooseTeam.jsx
@@ -6,28 +6,24 @@ import GameSetupStore from './gameSetupStore.js';
 export default React.createClass({
 
     render: function () {
-        let status = this.props.setupState.status;
-        let chosenSession = this.props.setupState.chosenSession;
-        if (status == GameSetupStore.GameSetupState.CHOOSE_TEAM && chosenSession) {
-            return (
-                <div id="chooseTeam">
-                    <h1 className="jumbotron">Joining Session {chosenSession.sessionName} ...</h1>
-                    <h2 className="jumbotron">Choose Your Preferred Team ... </h2>
-                    <div className="team-choice">
-                        <button type="button" name="team1" onClick= {() => chosenSession.joinSession(0)}>Team 1</button>
-                        &nbsp;&nbsp;&nbsp;
-                        <button type="button" name="team2" onClick={() => chosenSession.joinSession(1)}>Team 2</button>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <button type="button" name="teamAny" onClick={() => chosenSession.joinSession()}>Join Any Team</button>
-                    </div>
+        const status = this.props.setupState.status;
+        const chosenSession = this.props.setupState.chosenSession || {};
+
+        return (
+            <div id="chooseTeam" className={(status !== GameSetupStore.GameSetupState.CHOOSE_TEAM ? 'hidden' : '')}>
+                <h1 className="jumbotron">Joining Session {chosenSession.sessionName} ...</h1>
+                <h2 className = "jumbotron" > Choose Your Preferred Team ... </h2>
+                <div className="team-choice">
+                    <button type="button" name="team1" onClick= {() => chosenSession.joinSession(0)}>Team 1</button>
+                    &nbsp;&nbsp;&nbsp;
+                    <button type="button" name="team2" onClick={() => chosenSession.joinSession(1)}>Team 2</button>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <button type="button" name="teamAny" onClick={() => chosenSession.joinSession()}>Join Any Team</button>
                 </div>
-            );
-        }
-        else {
-            return (<div></div>);
-        }
+            </div>
+        );
     }
 
 });
