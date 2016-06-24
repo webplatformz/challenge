2c148b6a3c85eae2ef70261e6e843a1e07fa4633
r3dDoX <little_mc_walther@hotmail.com>
2016-06-24 08:49:57 +0200

    change to only add hidden class

--- client/js/gameSetup/waitForPlayers.jsx
+++ client/js/gameSetup/waitForPlayers.jsx
@@ -7,21 +7,17 @@ export default React.createClass({
 
     render: function () {
         let status = this.props.setupState.status;
-        let chosenSession = this.props.setupState.chosenSession;
-        if (status == GameSetupStore.GameSetupState.WAIT_FOR_PLAYERS) {
-            return (
-                <div id="waitForPlayers"
-                     className={(status !== GameSetupStore.GameSetupState.WAIT_FOR_PLAYERS ? 'hidden' : '')}>
+        let chosenSession = this.props.setupState.chosenSession || {};
+        return (
+            <div id="waitForPlayers"
+                 className={(status !== GameSetupStore.GameSetupState.WAIT_FOR_PLAYERS ? 'hidden' : '')}>
 
-                    <h1 className="jumbotron">Joining Session {chosenSession.sessionName} ...</h1>
+                <h1 className="jumbotron">Joining Session {chosenSession.sessionName} ...</h1>
 
-                    <h2 className="jumbotron">Waiting for players ... </h2>
+                <h2 className="jumbotron">Waiting for players ... </h2>
 
-                </div>
-            )
-        } else {
-            return (<div></div>)
-        }
+            </div>
+        );
     }
 
 });
