3a9917011dcdc4b31357f239f14c42c0882efc15
Daniel Stucki <daniel@stucki.me>
2016-06-27 16:49:19 +0200

    added react-addons-css-transition-group to dependencies

--- client/js/game/tableCards.jsx
+++ client/js/game/tableCards.jsx
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import CollectStichHint from './collectStichHint.jsx';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 import {GameState} from './gameStore';
 
 let cards =[];
@@ -17,12 +19,15 @@ export default React.createClass({
         return (
             <div id="tableCards">
                 {(this.props.collectStich === false) ? <CollectStichHint /> : undefined}
+                <ReactCSSTransitionGroup transitionName="cards" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
                 {cards.map((card, index) => {
                     let actPlayerIndex = (startingPlayerIndex + index) % 4;
                     return (
                         <img key={card.color + '_' + card.number} className={'card-' + playerSeating[actPlayerIndex]} src={imagePath + card.color.toLowerCase() + '_' + card.number + '.gif'} />
                     );
                 })}
+                </ReactCSSTransitionGroup>
+
             </div>
         );
     }
