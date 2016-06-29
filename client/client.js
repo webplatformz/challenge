601929eccf7d0338258d5a79f346a717a7986189
r3dDoX <little_mc_walther@hotmail.com>
2016-06-29 08:14:16 +0200

    remove unnecessary polyfills

--- client/client.js
+++ client/client.js
@@ -1,10 +1,5 @@
-'use strict';
-
-import './js/polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import JassApp from './js/jassApp.jsx';
 
-/*jshint ignore:start */
 ReactDOM.render(<JassApp />, document.getElementsByTagName('main')[0]);
-/*jshint ignore:end */
