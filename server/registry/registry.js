a6f9cbb06c686e744b819de2ea8995d05b80f0a0
nick <nick.payne89@gmail.com>
2016-06-29 11:05:30 +0200

    add gamemode and session name to registry api

--- server/registry/registry.js
+++ server/registry/registry.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import request from 'request';
-var http = require('http')
 
 let registryUrl = 'http://172.16.37.113:1338/api';
 
@@ -22,7 +21,15 @@ const Registry = {
 
     addBot(bot, wsUrl) {
         return new Promise((resolve, reject) => {
-            request.post({ url: registryUrl + '/addBot', json: true, body: { wsUrl: wsUrl, botId: bot.id }}, (err, res, body) => {
+            request.post({ url: registryUrl + '/addBot', json: true, body: {
+                wsUrl: wsUrl,
+                botId: bot.id,
+                mode: 'TOURNAMENT | SINGLE',
+                sessionName: 'sessionName'
+            }}, (err, res, body) => {
+                if (err) {
+                    return reject();
+                }
                 resolve();
             });
         });
