7276e2a9df31278ef6ea8e3363006fabd6f8a3ef
r3dDoX <little_mc_walther@hotmail.com>
2016-07-02 15:37:34 +0200

    add reducer templates for other stores

--- client/js/reducers/reducers.js
+++ client/js/reducers/reducers.js
@@ -1,8 +1,12 @@
 import {combineReducers} from 'redux';
 import tournament from './tournament';
 import jassApp from './jassApp';
+import gameSetup from './gameSetup';
+import singleGame from './singleGame';
 
 export default combineReducers({
+    gameSetup,
     jassApp,
-    tournament
+    tournament,
+    singleGame
 });
