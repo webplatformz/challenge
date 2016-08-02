5a5ac8abf158b3e1ee2cebfb4d5687c0b8862176
r3dDoX <patrick@walther-online.ch>
2016-08-02 14:18:50 +0200

    remove unneccessary braces ond return statement

--- client/js/gameSetup/chooseTeam.jsx
+++ client/js/gameSetup/chooseTeam.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import {GameSetupStep} from '../reducers/gameSetup';
 
-const ChooseTeam = ({step, chosenSession = {}}) => {
-
-    return (
-        <div id="chooseTeam" className={(step !== GameSetupStep.CHOOSE_TEAM ? 'hidden' : '')}>
-            <h1 className="jumbotron">Joining Session {chosenSession.sessionName}</h1>
-            <h2>Choose your preferred team</h2>
-            <div className="team-choice">
-                <button type="button" onClick={() => chosenSession.joinSession(0)}>Team 1</button>
-                <button type="button" onClick={() => chosenSession.joinSession(1)}>Team 2</button>
-                <button type="button" onClick={() => chosenSession.joinSession()}>Join any team</button>
-            </div>
+const ChooseTeam = ({step, chosenSession = {}}) => (
+    <div id="chooseTeam" className={(step !== GameSetupStep.CHOOSE_TEAM ? 'hidden' : '')}>
+        <h1 className="jumbotron">Joining Session {chosenSession.sessionName}</h1>
+        <h2>Choose your preferred team</h2>
+        <div className="team-choice">
+            <button type="button" onClick={() => chosenSession.joinSession(0)}>Team 1</button>
+            <button type="button" onClick={() => chosenSession.joinSession(1)}>Team 2</button>
+            <button type="button" onClick={() => chosenSession.joinSession()}>Join any team</button>
         </div>
-    );
-};
+    </div>
+);
 
 ChooseTeam.propTypes = {
     step: React.PropTypes.oneOf(Object.keys(GameSetupStep)),
