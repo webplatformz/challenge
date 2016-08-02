9bec869af22a58352ef521698e9112438b86147f
r3dDoX <patrick@walther-online.ch>
2016-08-02 14:18:02 +0200

    remove unneccessary braces ond return statement

--- client/js/gameSetup/connecting.jsx
+++ client/js/gameSetup/connecting.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import {GameSetupStep} from '../reducers/gameSetup';
 
-
-const Connecting = ({step}) => {
-    return (
-        <div id="connecting" className={(step !== GameSetupStep.CONNECTING ? 'hidden' : '')}>
-            <h1 className="jumbotron">Connecting to Server...</h1>
-        </div>
-    );
-};
+const Connecting = ({step}) => (
+    <div id="connecting" className={(step !== GameSetupStep.CONNECTING ? 'hidden' : '')}>
+        <h1 className="jumbotron">Connecting to Server...</h1>
+    </div>
+);
 
 Connecting.propTypes = {
     step: React.PropTypes.oneOf(Object.keys(GameSetupStep))
