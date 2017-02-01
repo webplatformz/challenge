43c729ae9c654bf7bc2e7241200128c27af5be6b
danielsuter <suter_daniel@hotmai.com>
2017-02-01 21:24:48 +0100

    formatting

--- client/js/tournament/rankingTable.jsx
+++ client/js/tournament/rankingTable.jsx
@@ -5,24 +5,25 @@ export default ({ranking = []}) => {
     const hasRating = ranking.length !== 0 && ranking[0].rating;
 
     return (
-    <table>
-        <thead>
-        <tr>
-            <th>Rank</th>
-            <th>Player</th>
-            {hasRating && <th>Rating (Glicko)</th>}
-            <th>Connected Clients</th>
-        </tr>
-        </thead>
-        <tbody>
-        {ranking.map((player) => (
-            <tr key={player.playerName}>
-                <td>{player.rank}</td>
-                <td>{player.playerName}</td>
-                {hasRating && <td>{Math.floor(player.rating)}</td>}
-                <td>{player.connectedClients}</td>
+        <table>
+            <thead>
+            <tr>
+                <th>Rank</th>
+                <th>Player</th>
+                {hasRating && <th>Rating (Glicko)</th>}
+                <th>Connected Clients</th>
             </tr>
-        ))}
-        </tbody>
-    </table>
-)};
+            </thead>
+            <tbody>
+            {ranking.map((player) => (
+                <tr key={player.playerName}>
+                    <td>{player.rank}</td>
+                    <td>{player.playerName}</td>
+                    {hasRating && <td>{Math.floor(player.rating)}</td>}
+                    <td>{player.connectedClients}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    )
+};
